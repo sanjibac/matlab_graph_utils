46f41a43143eb85b1e3ba316b740e4b72e53506c
Sanjiban Choudhury <sanjiban@cmu.edu>
2017-02-14 12:41:02 -0500

    more fun funcs

--- utils/plot_path.m
+++ utils/plot_path.m
@@ -1,8 +1,11 @@
-function plot_path( path, coord_set )
+function plot_path( path, coord_set, col )
 %UNTITLED Summary of this function goes here
 %   Detailed explanation goes here
 
+if(nargin <= 2)
+    col = 'b';
+end
 hold on;
-plot(coord_set(path, 1), coord_set(path, 2), 'b', 'LineWidth', 2);
+plot(coord_set(path, 1), coord_set(path, 2), col, 'LineWidth', 2);
 end
 
