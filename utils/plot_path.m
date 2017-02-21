4817a0eb7260c8a2d61e72ce26d6f61672b0c50b
Sanjiban Choudhury <sanjiban@cmu.edu>
2017-02-21 10:27:07 -0500

    lala

--- utils/plot_path.m
+++ utils/plot_path.m
@@ -1,11 +1,14 @@
-function plot_path( path, coord_set, col )
+function plot_path( path, coord_set, col, lw )
 %UNTITLED Summary of this function goes here
 %   Detailed explanation goes here
 
 if(nargin <= 2)
     col = 'b';
 end
+if(nargin <= 3)
+    lw = 2;
+end
 hold on;
-plot(coord_set(path, 1), coord_set(path, 2), col, 'LineWidth', 2);
+plot(coord_set(path, 1), coord_set(path, 2), col, 'LineWidth', lw);
 end
 
